a5c56ee4a75ddcb3aeb41f763571df1cf7d172dd
Sofie Gonzalez <33813152+sofiegonzalez@users.noreply.github.com>
2021-04-07 14:49:09 -0400

    first commit- simple fps color shooter

---
+++ fps/Assets/Scripts/crateColorChange.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crateColorChange : MonoBehaviour
+{
+    public Material start;
+    public Material red;
+    public Material blue;
+    public Material green;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        //starting color
+        GetComponent<MeshRenderer>().material = start;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeColor(string newColor)
+    {
+        //new color is the color the gun is set on
+        if(newColor == "green"){
+            //change object color when shot
+           
+            GetComponent<MeshRenderer>().material = green;
+        }else if (newColor == "red")
+        {
+            
+            GetComponent<MeshRenderer>().material = red;
+        }
+        else
+        {
+            
+            GetComponent<MeshRenderer>().material = blue;
+        }
+
+    }
+}
---
+++ fps/Assets/Scripts/gunChangeColor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunChangeColor : MonoBehaviour
+{
+    public float targetDistance = 0.0f;
+    public int allowedRange = 15;
+    public Material red;
+    public Material blue;
+    public Material green;
+
+    string color = "red";
+    public GameObject laser;
+    // Start is called before the first frame update
+    void Start()
+    {
+        laser.GetComponent<MeshRenderer>().material = red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //right click- change color
+        if (Input.GetMouseButtonDown(1))
+        {
+            if(color == "red")
+            {
+                color = "blue";
+                //change color of laser
+                laser.GetComponent<MeshRenderer>().material = blue;
+                Debug.Log("new color blue");
+            }
+            else if(color == "blue")
+            {
+                color = "green";
+                laser.GetComponent<MeshRenderer>().material = green;
+                Debug.Log("new color green");
+            }
+            else
+            {
+                color = "red";
+                laser.GetComponent<MeshRenderer>().material = red;
+
+            }
+        }
+            
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit shot;
+            if( Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out shot))
+            {
+                targetDistance = shot.distance;
+                if(targetDistance < allowedRange)
+                {
+                    //here we would change the color
+                    shot.transform.SendMessage("ChangeColor", color);
+                    
+                }
+                
+            }
+        }
+
+    }
+}
