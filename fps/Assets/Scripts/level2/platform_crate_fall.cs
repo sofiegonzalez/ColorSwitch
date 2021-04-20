31149448fb7d8659897a9599636f8d1b749691fd
Sofie Gonzalez <33813152+sofiegonzalez@users.noreply.github.com>
2021-04-20 17:39:02 -0400

    added to level2

---
+++ fps/Assets/Scripts/level2/platform_crate_fall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform_crate_fall : MonoBehaviour
+{
+    public string crate_color;
+    public crateColorChange crate;
+    private Collider myCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get current gun color
+        string currColor = crate.GetComponent<crateColorChange>().currColor;
+
+        //if player gun is on the same color as the cube platform, can walk on it
+        if (currColor == crate_color)
+        {
+            //Debug.Log("same");
+            //player can stand on cube
+            myCollider.enabled = true;
+        }
+        else
+        {
+            //player is unable to stand on platform if gun is not set to the same color
+            //Debug.Log("not same");
+            //player falls through cube
+            myCollider.enabled = false;
+        }
+    }
+
+    }
