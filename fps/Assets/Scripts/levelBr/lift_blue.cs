bdac23ea7b028acbc8838d0e4fa8497d8081d0f7
Andrew Bracken <abracke7@umd.edu>
2021-04-27 13:09:18 -0400

    full level

---
+++ fps/Assets/Scripts/levelBr/lift_blue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lift_blue : MonoBehaviour
+{
+    public GameObject pressure;
+    public Vector3 finalPos;
+    private pressurePlate c;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c = pressure.GetComponent<pressurePlate>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (c.solved)
+        {
+            StartCoroutine(SmoothLerp(1f));
+            c.solved = false;
+        }
+    }
+
+    //move blue up
+    private IEnumerator SmoothLerp(float time)
+    {
+        Vector3 startingPos = transform.position;
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
