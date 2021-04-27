8c30c49bd9d92c6830b7a3b6598eebf71bae1daf
Sofie Gonzalez <33813152+sofiegonzalez@users.noreply.github.com>
2021-04-27 16:46:06 -0400

    added Andy's level

---
+++ fps/Assets/Scripts/levelBr/next_level.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class next_level : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z >= 118)
+        {
+            SceneManager.LoadScene("level1"); // change to next level
+        }
+    }
+}
