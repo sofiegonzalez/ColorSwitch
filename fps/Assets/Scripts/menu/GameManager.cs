24af376aefa1f3241b1dddd1c2b6f6422243164e
Sofie Gonzalez <33813152+sofiegonzalez@users.noreply.github.com>
2021-05-06 11:42:34 -0400

    updates to ui

--- fps/Assets/Scripts/menu/GameManager.cs
+++ fps/Assets/Scripts/menu/GameManager.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
  
     void Update(){
  
     if(Input.GetKeyDown("escape"))
-        Application.Quit();
- 
+            SceneManager.LoadScene(0);
+
     }
  
 }
